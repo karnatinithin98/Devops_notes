1b3fbb86fafabc42a60ba6c522b794ce6669ce39
karnatinithin98 <karnatinithin98@gmail.com>
2023-01-17 13:34:45 +0000

    this is git notes

---
+++ git.py
@@ -0,0 +1,35 @@
+#Git branch : its shows which branch  
+#git status : main
+#Create a branch : with ticket Number
+
+#work on it and make changes
+
+#Git add .
+#git status
+#git commit -m "srw -40800 : adder kms-rhel any msg "
+
+
+#Git push Origin sre-40080
+
+#it will updated in github account
+
+#request a pull request and send to collegue and merge it
+
+#Git merge 
+
+------------------------------------------------------------------------------------------------------------
+
+#Git Steps to follow
+
+
+#1) open folder in vs code
+#2) Change the code 
+#3) git config --global user.name “karnatinithin98”
+#4) git config --global user.email “karnatinithin98@gmail.com”
+#5) git branch (master)
+#6) git branch test 
+#7) git checkout test ( swapped to test branch)
+#8) git status ( modification shows here) 
+#9) git add . ( Files will added)
+#10) git git remote add origin https://github.com/karnatinithin98/python.gi   ( Repo link)
+#11)git push -u origin test
